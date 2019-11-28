55d944aad4d0c0a0afc99791e345473a711f3c47
Luis B <luisbttf@gmail.com>
2019-11-27 20:08:31 -0600

    project v1

--- src/js/script.js
+++ src/js/script.js
@@ -8,8 +8,24 @@ $(function () {
 
 
 $(".owl-carousel").owlCarousel({
-	loop: true,
-	items: 1
+	autoplay: true,
+	items: 1,
+  singleItem: true,
+  slideSpeed: 2000,
+  theme: 'owl-theme',
+	pagination:false,
+	navigation:true,
+  dots: false,
+  loop: true,
+  nav: true
 });
+
+
+$(".owl-prev span").html('');
+$(".owl-next span").html('');
+
+
+
+
 
 });
